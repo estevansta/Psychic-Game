c0de7f2a6c3b5e1fc1675a98e6b20e660220e8ba
Estevan Cruz <Estevancruz@Estevans-MBP.lan>
2018-12-11 11:24:09 -0600

    onkey event added: user guesses, computer guess with math.floor/random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// Variables //
+
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
@@ -5,4 +7,23 @@ var losses = 0;
 var numGuesses = 9;
 var guessChoices = [];
 
+// onkey evenets //
 
+document.onkeyup = function(event) {
+
+    var userGuesses = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+
+
+
+
+}
+
+
+//console log test//
+
+console.log(wins, losses, numGuesses);
